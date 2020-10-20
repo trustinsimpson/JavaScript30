416332abafd44dd7cf327a0cd513203a8542cfbc
Trustin Simpson <contact@trustinsimpson.com>
2020-10-20 17:07:15 -0500

    finished exercise 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,61 @@
+/* grab elements */
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* functions */
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚'; 
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+/* listeners */
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+toggle.addEventListener('keyup', togglePlay => {
+  if (togglePlay.keyCode === 32) {
+    return;
+  }
+});
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
